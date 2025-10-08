2b10365b933e248f224a9b272738c3d8fb5cdca9
Keidbyr <117668622+Keidbyr@users.noreply.github.com>
2025-10-08 23:23:00 +0500

    Migrations task

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->date('date_of_birth');
+            $table->integer('limit_of_books');
+            $table->integer('reputation');
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2025_11_08_172757_create_copies_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('copies', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('book_id')->constrained()->onDelete('cascade');
+            $table->boolean('integrity');
+            $table->boolean('in_stock');
+            $table->foreignId('library_id')->constrained('libraries');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('copies');
+    }
+};
